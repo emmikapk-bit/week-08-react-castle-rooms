5a619e1f72bbccd60f3f2a6af719b58b213df8f6
emmikapk-bit <emmika.pk@gmail.com>
2026-05-03 01:42:35 +0700

    added_count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,21 +1,35 @@
+import { useState } from "react";
+
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => setCount(count + 1);
+  const handleDecrease = () => setCount(count - 1);
+  const handleReset = () => setCount(0);
+
   return (
     <div className="flex flex-col justify-center items-center bg-gray-800 w-full">
       <h1 id="count" className="mb-6 text-5xl">
-        0
+        {count}
       </h1>
       <div className="flex gap-3">
         <button
-          id="Plus1"
-          className="px-4 py-1 h-9 rounded-xl bg-white text-black hover:bg-amber-300 hover:text-black "
-        >
-          Click +1
-        </button>
-        <button
-          id="Minus1"
+          onClick={handleDecrease}
           className="px-4 py-1 h-9 rounded-xl bg-white text-black hover:bg-amber-300 hover:text-black "
         >
           Click -1
+        </button>
+        <button
+          onClick={handleReset}
+          className="px-4 py-1 h-9 rounded-xl bg-black text-white hover:bg-amber-300 hover:text-black "
+        >
+          Reset
+        </button>
+        <button
+          onClick={handleIncrease}
+          className="px-4 py-1 h-9 rounded-xl bg-white text-black hover:bg-amber-300 hover:text-black "
+        >
+          Click +1
         </button>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center bg-gray-800 w-full h-full">
       <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
-      <p className="text-xl">This is the home page of our React application.</p>
+      <p className="text-xl mb-5">
+        This is the home page of our React application.
+      </p>
       <img
         src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJueGZ3bmZ3bmZ3bmZ3bmZ3bmZ3/3o7TKMGpxx660738P6/giphy.gif"
         alt="Castle"
